fbb84b5f51278a1122331fee2049e834739b76cb
denisskripnik <valis.source@gmail.com>
2021-12-11 13:12:16 +0200

    1

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const serveStatic = require("serve-static")
+const cors = require('cors')
+
 const path = require('path');
 app = express();
+app.use(cors())
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
